e19840d108518eb5eb6357c5ce4d9b58edf7dced
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-09-20 19:36:04 +0400

    c

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const tabBtn = document.getElementById("tab-btn")
+
+let myLeads = []
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
